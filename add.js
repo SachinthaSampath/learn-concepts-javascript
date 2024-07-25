6957acf73fd24cbfd018777419546b387a050a91
Sachintha Sampath <sachinsampath42@gmail.com>
2024-07-26 01:36:30 +0530

    feat: common-js

--- add.js
+++ add.js
@@ -1,5 +1,25 @@
-(function () {
-    myApp.add = function (a, b) {
-        return a + b;
-    }
-})();
+// module.exports = function add(a, b) {
+//     return a + b;
+// }
+
+// module.exports = function subtract(a, b) {
+//     return a - b;
+// }
+
+
+// In CommonJS syntax, you can't use module.exports multiple times to export different functions because each assignment will overwrite the previous one. Instead, you should export an object containing all the functions you want to export.
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    add,
+    subtract
+};
